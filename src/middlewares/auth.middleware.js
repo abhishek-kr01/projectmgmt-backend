74f2eba63942cce7a145ca82e9ec6fe564763f6d
Abhishek Kumar <abhishekmdp11@gmail.com>
2026-04-25 00:05:44 +0530

    add validators and RBAC permission middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,13 +1,15 @@
+import mongoose from "mongoose";
+import { ProjectMember } from "../models/projectmember.models.js";
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import jwt from "jsonwebtoken"
 
 
-export const verifyJWT = asyncHandler(async(req, res, next) => {
+export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
-    if(!token){
+    if (!token) {
         throw new ApiError(401, "Unauthorized request")
     }
 
@@ -17,12 +19,44 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
             "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
         )
 
-        if(!user){
-        throw new ApiError(401, "Invalid access token")
+        if (!user) {
+            throw new ApiError(401, "Invalid access token")
         }
         req.user = user
         next()
     } catch (error) {
         throw new ApiError(401, "Invalid access token")
     }
-})
+});
+
+export const validateProjectPermission = (roles = []) => {
+    asyncHandler(async (req, res, next) => {
+        const { projectId } = req.params;
+
+        if (!projectId) {
+            throw new ApiError(400, "Project id is missing");
+        }
+
+        const project = await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id)
+        });
+
+        if (!project) {
+            throw new ApiError(400, "Project not found!");
+        }
+
+        const givenRole = project?.role;
+
+        req.user.role = givenRole;
+
+        if (!roles.includes(givenRole)) {
+            throw new ApiError(
+                403,
+                "You do not have permission to perform this action"
+            )
+        }
+        
+        next();
+    })
+};
--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,5 @@
 import { body } from 'express-validator'
+import { AvailableUserRole } from '../utils/constants.js'
 
 
 const userRegisterValidator = () => {
@@ -15,7 +16,7 @@ const userRegisterValidator = () => {
             .withMessage("Username is required")
             .isLowercase()
             .withMessage("Username must be in lower case")
-            .isLength({min: 3})
+            .isLength({ min: 3 })
             .withMessage("Username must be at least 3 characters long"),
         body("password")
             .trim()
@@ -68,10 +69,38 @@ const userResetForgotPasswordValidator = () => {
     ]
 }
 
+const createProjectValidator = () => {
+    return [
+        body("name")
+            .notEmpty()
+            .withMessage("Name is required"),
+        body("description")
+            .optional()
+    ];
+};
+
+const addMemberToProjectValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("role")
+            .notEmpty()
+            .withMessage("Role is required")
+            .isIn(AvailableUserRole)
+            .withMessage("Role is invalid"),
+    ]
+}
+
 export {
     userRegisterValidator,
     userLoginValidator,
     userChangeCurrentPasswordValidator,
     userForgotPasswordValidator,
-    userResetForgotPasswordValidator
+    userResetForgotPasswordValidator,
+    createProjectValidator,
+    addMemberToProjectValidator
 }
